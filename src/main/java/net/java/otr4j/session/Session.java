f104950cc88bb301082fceea08ae4e4d9f753929
Danny van Heumen <danny@dannyvanheumen.nl>
2016-01-25 23:19:52 +0100

    Fixed interoperability issue for receiver instance IDs > 0x7fffffff.
    NumberFormatException caused instance IDs to be invalid:
    Integer.parseInt(...) expects signed value and effectively only parses
    up to 31-bit integer values.

--- src/main/java/net/java/otr4j/session/Session.java
+++ src/main/java/net/java/otr4j/session/Session.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.crypto.interfaces.DHPublicKey;
@@ -350,7 +351,7 @@ public class Session {
             getHost().messageFromAnotherInstanceReceived(getSessionID());
             return null;
         } catch (ProtocolException e) {
-            logger.warning("An invalid message fragment was discarded.");
+            logger.log(Level.WARNING, "An invalid message fragment was discarded.", e);
             return null;
         }
 
