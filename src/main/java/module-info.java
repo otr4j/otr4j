4b330efdc6b33e396ed097252d7fd75881867ef0
Danny van Heumen <danny@dannyvanheumen.nl>
2024-09-06 17:53:07 +0200

    java: fix module definition to export necessary components

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,19 @@
  * Definition of the otr4j module.
  */
 // TODO `require static` for automatic modules. Ideally, these dependencies define their own module.
-module otr4j {
-    requires static jsr305;
-    requires static com.google.errorprone.annotations;
+module net.java.otr4j {
+    requires static transitive jsr305;
+    requires static transitive com.google.errorprone.annotations;
 
+    requires transitive joldilocks;
     requires java.logging;
-    requires joldilocks;
     requires org.bouncycastle.provider;
+    
+    exports net.java.otr4j.api;
+    exports net.java.otr4j.crypto;
+    exports net.java.otr4j.crypto.ed448;
+    exports net.java.otr4j.io;
+    exports net.java.otr4j.messages;
+    exports net.java.otr4j.session;
+    exports net.java.otr4j.util;
 }
